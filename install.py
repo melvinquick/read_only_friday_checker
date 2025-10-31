ec3923c060ed96b2b1928ff734be6d118e5bf146
melvinquick <melvinquick@proton.me>
2025-10-31 15:21:52 -0400

    Reduce required Python version to 3.10

--- install.py
+++ install.py
@@ -103,7 +103,7 @@ def install():
     venv_path = get_venv_path()
     create_venv(venv_path)
     install_app(venv_path)
-    version = "1.0.2"
+    version = "1.0.3"
     icon = get_icon(venv_path)
     python = get_python_path(venv_path)
     app = get_app_path(venv_path)
